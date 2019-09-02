cc011e4359c62a69f569510dcf5affab31b71f80
Phil Swartz <pswartz.sju@gmail.com>
2019-09-02 08:23:10 -0400

    added an on click event to the red gem in the game and stored it's random value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,23 @@
 // constraints = 19 - 120
 var randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
 
-
-
-
+var totalScore = 0;
+var scoreCounter = "";
 var targetNumber = [];
+
+var redGemRandNum = Math.floor(Math.random() * (12 - 1) + 1);
+var greenGemRandNum = Math.floor(Math.random() * (12 - 1) + 1);
+var blueGemRandNum = Math.floor(Math.random() * (12 - 1) + 1);
+var blueDiamondRandNum = Math.floor(Math.random() * (12 - 1) + 1);
+
+
+console.log(redGemRandNum);
+console.log(greenGemRandNum);
+console.log(blueGemRandNum);
+console.log(blueDiamondRandNum);
+
+
+
+
+
+
